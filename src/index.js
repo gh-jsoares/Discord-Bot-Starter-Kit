795cd920a4e687ae97fee6a4a8ebc8e82172722b
João Soares <jsoares.public@gmail.com>
2020-06-01 10:03:23 +0100

    Added pg as driver and knex for making queries. Custom orm incoming

--- src/index.js
+++ src/index.js
@@ -1,4 +1,15 @@
-import { TOKEN } from '../config/config.js';
+import { TOKEN, DATABASE, DATABASE_DRIVER } from '../config/config.js';
 import { Bot } from './bot/client.js';
+import knex from 'knex';
 
-Bot.run({ token: TOKEN });
+const connection = knex({
+    client: DATABASE_DRIVER,
+    connection: DATABASE
+});
+
+connection.raw('SELECT 1').then(() => {
+    console.log('Connected to database.');
+    Bot.run({ token: TOKEN });
+}).catch((err) => {
+    console.error(`${err.severity} ERROR: ${err.message}`);
+});
